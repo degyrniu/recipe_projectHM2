1e38fe55476e503c2686ae49ad32fff2d05029a2
degyrniu <w582mtwfnk@privaterelay.appleid.com>
2026-06-03 11:06:44 +0300

    Добавил класс Recipe

--- recipes.py
+++ recipes.py
@@ -29,3 +29,52 @@ class Ingredient:
         if isinstance(other, Ingredient) and self.name == other.name and self.unit == other.unit:
             return True
         return False
+
+
+
+class Recipe:
+    def __init__(self, title, ingredients=None):
+        self.title = title
+        if ingredients is None:
+            self.ingredients = []
+        else:
+            self.ingredients = ingredients
+
+
+    def add_ingredient(self, ingredient: Ingredient):
+        flag = False
+        for el in self.ingredients:
+            if el == ingredient:
+                el.quantity += ingredient.quantity
+                flag = True
+                break
+        if not flag:
+            self.ingredients.append(ingredient)
+
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+
+
+    def scale(self, ratio: float):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError
+        new_recipe = Recipe(self.title)
+        for el in self.ingredients:
+            new_ingredient = Ingredient(el.name, el.quantity * ratio, el.unit)
+            new_recipe.add_ingredient(new_ingredient)
+        return new_recipe
+
+
+    def __len__(self):
+        return len(self.ingredients)
+
+
+    def __str__(self):
+        st = f"{self.title}:\n"
+        for el in self.ingredients:
+            st += f"{el}\n"
+        return st.strip()
+
+
