7f3b18e386428b99dca259f8a4a31d5d35cf6030
degyrniu <w582mtwfnk@privaterelay.appleid.com>
2026-06-03 11:29:29 +0300

    Класс ShoppingList готов

--- recipes.py
+++ recipes.py
@@ -78,3 +78,52 @@ class Recipe:
         return st.strip()
 
 
+
+class ShoppingList:
+    def __init__(self):
+        self._items = []
+
+
+    def add_recipe(self, recipe: Recipe, portions: float):
+        if portions <= 0:
+            raise ValueError("Количество порций должно быть положительным")
+        new_recipe = recipe.scale(portions)
+        for ingredient in new_recipe.ingredients:
+            self._items.append((ingredient, recipe.title))
+
+
+    def remove_recipe(self, title: str):
+        items = []
+        for ingredient, recipe_title in self._items:
+            if recipe_title != title:
+                items.append((ingredient, recipe_title))
+        self._items = items
+
+
+    def get_list(self):
+        res = {}
+        for ingredient, recipe_title in self._items:
+            key = (ingredient.name, ingredient.unit)
+            if key in res:
+                res[key] += ingredient.quantity
+            else:
+                res[key] = ingredient.quantity
+        shopping_list = []
+        for key, quantity in res.items():
+            name, unit = key
+            shopping_list.append(Ingredient(name, quantity, unit))
+        shopping_list.sort(key=lambda ingredient: ingredient.name)
+        return shopping_list
+
+
+    def __add__(self, other):
+        new_list = ShoppingList()
+        for item in self._items:
+            new_list._items.append(item)
+        for item in other._items:
+            new_list._items.append(item)
+        return new_list
+
+
+
+
